d5fbba88c6df27e9ebcc60d56b7072eff3bbafb0
Alemenew Dubie Tarekegn <alexdubie333@gmail.com>
2026-05-02 15:54:01 +0300

    Delete Electric Billing System directory

--- Electric Billing System/ElectricBillingSystem/save_background.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Save your background image as:
-echo   ElectricBillingSystem\resources\background.jpg
-echo.
-echo The system will use a dark blue gradient fallback if the image is not found.
-pause
